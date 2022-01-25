1f121ce79816c11b72e822c6112e4e95c8d50d6f
Carl Hester <carlhester@gmail.com>
2022-01-24 19:08:10 -0800

    wip

--- static/data.js
+++ static/data.js
@@ -1,44 +1,50 @@
-const data = function () {
-    console.log("data called")
+const t = function () {
+    console.log("t called")
+}
+
+
+const callDataAPI = function () {
     var httpRequest;
 
     function makeRequest() {
         httpRequest = new XMLHttpRequest();
-        httpRequest.onreadystatechange = getContents();
+        httpRequest.onreadystatechange = getContents;
         httpRequest.open("GET", "/data");
         httpRequest.send();
-    }
+    };
+
+    makeRequest();
 
     function getContents() {
         const queryTable = document.querySelector("#queries");
         var respData = "";
 
         if (httpRequest.readyState === XMLHttpRequest.DONE) {
+            console.log("READY!")
             if (httpRequest.status === 200) {
                 respData = JSON.parse(httpRequest.responseText);
+                console.log(respData);
                 var row = queryTable.insertRow(1);
                 var c1 = row.insertCell(0);
                 var c2 = row.insertCell(1);
 
                 c1.innerHTML = respData["Link"];
-                c2.innerHTML = respData["Tags"][0] + "," + respData["Tags"][1];
-
-                console.log(respData);
+                c2.innerHTML = `<a class="btn btn-primary btn-sm">${respData["Tags"][0]}</a> <a class="btn btn-primary btn-sm">${respData["Tags"][1]}</a>`
             }
         }
     }
 }
 
-const add = function () {
+const callAddAPI = function () {
     console.log("add called")
     var httpRequest;
 
-    function submitData() {
+    (function () {
         httpRequest = new XMLHttpRequest();
         httpRequest.onreadystatechange = getContents();
         httpRequest.open("POST", "/add");
         httpRequest.send();
-    }
+    })();
 
     function getContents() {
         if (httpRequest.readyState === XMLHttpRequest.DONE) {
@@ -52,5 +58,6 @@ const add = function () {
     }
 }
 
-document.getElementById("ajaxButton").addEventListener("click", data());
-document.getElementById("btnSubmitNew").addEventListener("click", add());
+
+document.getElementById("ajaxButton").addEventListener("click", callDataAPI);
+document.getElementById("btnSubmitNew").addEventListener("click", callAddAPI);
